92794334424d78fb8cf4e482bdd190a96e8214bd
yohanis-ke <yohanes.ke@gmail.com>
2018-08-08 16:07:08 -0400

    have finished the my first rail aplication

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -47,4 +47,10 @@ class PicturesController < ApplicationController
     end
   end
 
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   get 'pictures/:id/edit' => "pictures#edit"
   patch 'pictures/:id' => "pictures#update"
 
+  delete 'pictures/:id' => 'pictures#destroy'
+
 end
