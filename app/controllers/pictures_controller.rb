c4955e215f37ba77db0084ff5291061e3e961e8b
yohanis-ke <yohanes.ke@gmail.com>
2018-08-08 14:22:26 -0400

    creating form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,17 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+     render text: "Received POST request to '/pictures' with the data URL: #{params[:id]}"
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pictures' => 'pictures#index'
+
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new' => 'pictures#new'
+
+  get 'pictures/:id' => 'pictures#create'
 end
