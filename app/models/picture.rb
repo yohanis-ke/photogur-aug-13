54ef0f9cd72cc176811c5df8f75287f0173d17c7
yohanis-ke <yohanes.ke@gmail.com>
2018-08-14 15:03:19 -0400

    Monday, Aug 13th 03 - Validations for Photogur

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,5 @@
 class Picture < ApplicationRecord
+  validates :artist, presence: true
+  validates :title, length: {minimum: 3, maximum: 20}
+  validates :url, presence: true, uniqueness: true
 end
